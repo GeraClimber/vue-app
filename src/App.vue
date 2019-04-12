97681ba290da9570ad9eaa2b700d2c323947c2af
Gennady Glushenkov <gennadiyglushenkov@gmail.com>
2019-04-12 12:50:56 +0300

    add vue-router and test pages all is good

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div id="app">
-    <h1>hello-vue</h1>
+  <div id="app" class="container">
+ <router-view></router-view>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app',
-  components: {
-  }
+ 
 }
 </script>
 
